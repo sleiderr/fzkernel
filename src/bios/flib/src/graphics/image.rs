e89ce15c995522e9aa26548102b3cfd52dcf29f3
Raphaël Faure <raphael.faure2@student-cs.fr>
2023-07-24 16:34:53 +0200

    feat: Implemented drawing tools

---
+++ src/bios/flib/src/graphics/image.rs
@@ -0,0 +1,103 @@
+use crate::data::r#abstract::{DataSegment, DataSource, Disk};
+use crate::disk_io::disk::AddressPacket;
+use crate::video_io::io::{switch_graphic, write};
+use core::cmp::max;
+use core::mem::size_of;
+use core::ptr::read_volatile;
+use numtoa::NumToA;
+
+#[repr(C, packed)]
+pub struct Image {
+    dim: (u16, u16),
+}
+
+pub struct Point {
+    x: u16,
+    y: u16,
+}
+
+#[repr(C, packed)]
+struct Pixel {
+    x: u16,
+    y: u16,
+    color: u8,
+}
+
+impl Point {
+    pub fn x(&self) -> u16 {
+        self.x
+    }
+
+    pub fn y(&self) -> u16 {
+        self.y
+    }
+}
+
+impl Image {
+    pub fn draw<S: DataSource>(&self, center: Point, source: S, origin: u32) -> Result<(), ()> {
+        switch_graphic();
+        let max = max(self.x_size(), self.y_size());
+        let n_pixel = self.dim.0 as u64 * self.dim.1 as u64;
+        let mut image_segment: DataSegment<Pixel, S> = DataSegment::default(source);
+        image_segment.set_length(2560);
+        image_segment.set_physical_origin(origin);
+        let mut address = 0x4;
+        for pixel in 0..n_pixel {
+            if let Some(pixel) = image_segment.read_abstract(address) {
+                write(
+                    pixel.x + center.x() - self.x_size() * (640 / max) / 2,
+                    pixel.y + center.y() - self.y_size() * (480 / max) / 2,
+                    pixel.color,
+                );
+            } else {
+                return Err(());
+            }
+            address += size_of::<Pixel>() as u32;
+        }
+        Ok(())
+    }
+
+    pub fn x_size(&self) -> u16 {
+        self.dim.0
+    }
+
+    pub fn y_size(&self) -> u16 {
+        self.dim.1
+    }
+
+    pub fn new(dim: (u16, u16)) -> Self {
+        Self { dim }
+    }
+}
+
+impl Point {
+    pub fn new(x: u16, y: u16) -> Self {
+        return Self { x, y };
+    }
+}
+
+impl From for Image {
+    fn from_address(address: u32) -> Self {
+        let mut address = address;
+        let x = unsafe { read_volatile(address as *const u16) };
+        address += 2;
+        let y = unsafe { read_volatile(address as *const u16) };
+
+        Self { dim: (x, y) }
+    }
+
+    fn from_disk(block: u64, buffer: u32) -> Result<Self, ()> {
+        let address = AddressPacket::new(1, buffer, block);
+        match address.disk_read(0x80) {
+            Ok(_) => Ok(Self::from_address(buffer)),
+            Err(_) => Err(()),
+        }
+    }
+}
+
+pub trait From {
+    fn from_address(address: u32) -> Self;
+    fn from_disk(block: u64, buffer: u32) -> Result<Self, ()>
+    where
+        Self: Sized;
+}
--- src/bios/flib/src/video_io/io.rs
+++ src/bios/flib/src/video_io/io.rs
@@ -15,12 +15,30 @@ pub fn cprint_info(str: &[u8]) {
     }
 }
 
+pub fn write(x: u16, y: u16, color: u8) {
+    unsafe {
+        asm!(
+        "pusha",
+        "mov ah, 0x0c",
+        "mov bh, 0x00",
+        "int 0x10",
+        "popa",
+        in("dx") y,
+        in("cx") x,
+        in("al") color
+        )
+    }
+}
 
 pub fn __bios_printc(ch: u8) {
-    let reg : u16 = u16::from(ch) | 0x0e00;
+    let reg: u16 = u16::from(ch) | 0x0e00;
     unsafe {
         asm!("push bx", "mov bx, 0", "int 0x10", "pop bx", in("ax") reg);
     }
+}
+
+pub fn switch_graphic() -> () {
+    unsafe { asm!("pusha", "mov ah, 0x00", "mov al, 0x12", "int 0x10", "popa") }
 }
 
 pub fn __bios_print(args: fmt::Arguments) {
@@ -36,24 +54,17 @@ pub fn __bios_print_str(s: &str) {
 }
 
 pub fn color() {
-
-    unsafe {
-        asm!(
-        "mov ah, 0x0b",
-        "xor bh, bh",
-        "mov bl, 0x01"
-        )
-    }
+    unsafe { asm!("mov ah, 0x0b", "xor bh, bh", "mov bl, 0x01") }
 }
 
 pub fn clear_screen() {
     unsafe {
         asm!(
-        "push ax",
-        "mov ah, 0x00",
-        "mov al, 0x03",
-        "int 0x10",
-        "pop ax"
+            "push ax",
+            "mov ah, 0x00",
+            "mov al, 0x03",
+            "int 0x10",
+            "pop ax"
         )
     }
 }
