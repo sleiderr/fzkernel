1e41371646a780f851b5d01d836dd96ed0ae9c36
Sacha <sacha.dupuydauby@student-cs.fr>
2023-07-30 23:37:29 +0200

    feat(prot-mode): Update segment registers for protected mode

--- src/bios/f-init/src/pswitch/mod.rs
+++ src/bios/f-init/src/pswitch/mod.rs
@@ -1,1 +1,25 @@
+use core::{arch::asm, hint::unreachable_unchecked};
+
 pub mod a20;
+pub mod gdt;
+
+pub fn protected_jump() -> ! {
+    unsafe {
+        asm!(
+            "mov ax, 0x08",
+            "mov ds, ax",
+            "mov es, ax",
+            "mov fs, ax",
+            "mov gs, ax",
+            "mov ss, ax",
+            "mov eax, cr0",
+            "or eax, 1",
+            "mov cr0, eax",
+            /*".byte 0x66",
+            ".byte 0xea",
+            ".int 0x01000000",
+            ".word 0x10"*/
+        );
+        unreachable_unchecked();
+    }
+}
