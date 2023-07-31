72b26641ae4c5cd5dfd7d04324aaaf4f0596ffb9
Sacha <sacha.dupuydauby@student-cs.fr>
2023-07-31 17:57:38 +0200

    fix(prot-mode): remove redundant function

--- src/bios/f-init/src/pswitch/mod.rs
+++ src/bios/f-init/src/pswitch/mod.rs
@@ -2,25 +2,3 @@ use core::{arch::asm, hint::unreachable_unchecked};
 
 pub mod a20;
 pub mod gdt;
-
-pub fn protected_jump() -> ! {
-    unsafe {
-        asm!(
-            "mov ax, 0x08",
-            "mov ds, ax",
-            "mov es, ax",
-            "mov fs, ax",
-            "mov gs, ax",
-            "mov ss, ax",
-            "mov eax, cr0",
-            "or eax, 1",
-            "mov cr0, eax",
-            "mov eax, 2",
-            ".byte 0x66",
-            ".byte 0xea",
-            ".int 0x5e00",
-            ".word 0x10"
-        );
-        unreachable_unchecked();
-    }
-}
