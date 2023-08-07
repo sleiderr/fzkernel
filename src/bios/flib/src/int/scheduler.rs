4ebd51a1b08af4f6ccf094d859433d45caa42831
Raphaël Faure <raphael.faure2@student-cs.fr>
2023-08-07 16:47:15 +0200

    Scheduler prototype

---
+++ src/bios/flib/src/int/scheduler.rs
@@ -0,0 +1,26 @@
+use alloc::collections::VecDeque;
+use core::mem::transmute;
+
+pub struct ScheduledAction {
+    action : usize,
+}
+
+impl ScheduledAction {
+    /// Creates a new [`ScheduledAction`] given a value
+    pub fn new(value : usize) -> Self {
+        Self {
+            action: value,
+        }
+    }
+}
+
+pub struct IntScheduler {
+    queue : VecDeque<ScheduledAction>
+}
+
+impl IntScheduler {
+    /// Schedules a given action by pushing it in the queue
+    pub fn schedule(&mut self, action : ScheduledAction) {
+        self.queue.push_back(action);
+    }
+}
