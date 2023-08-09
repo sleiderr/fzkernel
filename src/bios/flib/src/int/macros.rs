2aa90ce91ff010e7f57263379846a89b173e3c45
Raphaël Faure <raphael.faure2@student-cs.fr>
2023-08-09 16:25:06 +0200

    Remove scheduler_ref macro (useless)

--- src/bios/flib/src/int/macros.rs
+++
@@ -1,9 +0,0 @@
-
-/// Set up the global Scheduler in a given variable
-#[macro_export]
-macro_rules! scheduler_ref {
-    ($scheduler:ident) => {
-        let mut $scheduler: &mut Box<$crate::int::scheduler::IntScheduler> = unsafe { transmute(SCHEDULER_ADDRESS) };
-    }
-}
-
