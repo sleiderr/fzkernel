3cdeca0379ada2e451d0b5aa98902d7188a79ae9
Raphaël Faure <raphael.faure2@student-cs.fr>
2023-08-09 16:33:41 +0200

    Improve macros

--- src/bios/f-macros/src/lib.rs
+++ src/bios/f-macros/src/lib.rs
@@ -8,9 +8,8 @@ use std::path::PathBuf;
 
 use proc_macro2::{Span, TokenStream};
 use quote::quote;
-use syn::{Ident, Item, ItemFn, parse_file, parse_macro_input, Stmt};
-use syn::__private::str;
 use syn::spanned::Spanned;
+use syn::{parse_file, parse_macro_input, Ident, Item, ItemFn, Stmt};
 
 /// This procedural macro will generate a function that will build the IDT from
 /// the module where all interrupts are defined.
@@ -52,7 +51,7 @@ pub fn interrupt_descriptor_table(
     let file = fs::read_to_string(&path).unwrap();
     let code = parse_file(&file).unwrap();
     let mut interrupts_token: Vec<TokenStream> = Vec::new();
-
+    // Iterate over all 256 interrupts
     for i in 0..256 {
         let title = format!("_int0x{:x}", i);
         let title = title.as_str();
@@ -62,13 +61,12 @@ pub fn interrupt_descriptor_table(
             syn::__private::Span::mixed_site(),
         );
         let fn_name = Ident::new(&title, Span::mixed_site());
-        // Modify offset of entry in table
+        // Add statements to set handler's address for this entry in the IDT
         let code = quote! {
-                        let #ident = table.get_entry_mut(#int_number).unwrap();
-                        #ident.set_offset(interrupts::handlers::#fn_name as *const () as *const u8 as u32);
-                    };
+            let #ident = table.get_entry_mut(#int_number).unwrap();
+            #ident.set_offset(interrupts::handlers::#fn_name as *const () as *const u8 as u32);
+        };
         interrupts_token.push(code);
-
     }
 
     let default_table = quote! {
@@ -84,27 +82,40 @@ pub fn interrupt_descriptor_table(
             .with_segment_index(16);
         default.set_segment_selector(segment);
         default.set_valid();
-
         // We populate the table
         table.populate(default);
     };
 
+    // We merge every streams.
     let stream = quote! {
         #item_2
-        /// Function name
+        // Function name
         fn generate_idt() {
             #default_table
             #(#interrupts_token)*
             table.write(#offset);
         }
     };
-
     stream.into()
 }
 
 /// This proc macro aims to provide a higher level interface for interrupts definition.
-/// It will soon support gate type parameter in order to adapt asm! instructions for
-/// specific cases.
+///
+/// This macro wraps the function in a naked wrapper function.
+/// The wrapper function is defined as follows :
+/// ```rust
+/// use std::arch::asm;
+///
+/// #[naked]
+/// pub fn _foo_naked_wrapper () {
+///     unsafe { asm!(
+///         "pushad
+///         call _foo
+///         popad
+///         iretd", options(noreturn)
+///     ) }
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn interrupt(
     args: proc_macro::TokenStream,
@@ -122,32 +133,53 @@ pub fn interrupt(
     } = func;
 
     match arg {
-        /// Handle specific arg (not implemented yet)
+        // Handle specific arg (not implemented yet)
         _ => {
-            let fn_ident = sig.ident.to_string().replace("_", ".");
-            let title = fn_ident.to_string();
+            let name = sig.ident.to_string();
+            let wrapped_name = format!("_wrapped{}", name);
+
             /// Compute int number
             let int_number: u32 =
-                u32::from_str_radix(title.split("x").collect::<Vec<&str>>().get(1).unwrap(), 16)
+                u32::from_str_radix(name.split("x").collect::<Vec<&str>>().get(1).unwrap(), 16)
                     .unwrap();
             let default_ident = Ident::new(&"int_n", Span::mixed_site());
             let body = &block.stmts;
-            /// We had custom asm! at the end to return properly
-            let stream = quote! {
+            let wrapped_ident = Ident::new(wrapped_name.as_str(), Span::mixed_site());
+            // Rename routine to wrap it
+            let wrapped = quote! {
                 #[no_mangle]
-                #[link_section = #fn_ident]
-                #(#attrs)* #vis #sig {
-                    let #default_ident : u32 = #int_number;
+                #[link_section = ".int"]
+                pub fn #wrapped_ident () {
                     #(#body)*
-                    unsafe {
-                        asm!(
-                            "iret"
-                        )
-                    };
                 }
             };
 
-            //panic!("{}", stream.to_string());
+            // Define wrapper assembly
+            let wrapper = format!(
+                "pushad
+                call {}
+                popad
+                iretd",
+                wrapped_name
+            );
+
+            let wrapper_ident = Ident::new(&name, Span::mixed_site());
+
+            let wrapper = quote! {
+                #[naked]
+                pub fn #wrapper_ident () {
+                    unsafe {
+                        asm!(
+                            #wrapper
+                        , options(noreturn))
+                    }
+                }
+            };
+
+            let stream = quote! {
+                #wrapped
+                #wrapper
+            };
             stream.into()
         }
     }
@@ -166,10 +198,11 @@ pub fn interrupt_default(
         sig,
         block,
     } = default;
-    /// Compute default body
+    // Compute default body
     let default_body = block.stmts;
-    /// We check which interrupts are implemented in handlers.rs
+    // We check which interrupts are implemented in handlers.rs
     let span = proc_macro::Span::call_site();
+    // List of all already implemented interrupt to prevent overwriting them
     let mut int_defined = Vec::new();
     let source = span.source_file();
     let path = source.path();
@@ -180,13 +213,16 @@ pub fn interrupt_default(
             Item::Fn(f) => {
                 if f.sig.ident != sig.ident {
                     let title = f.sig.ident.to_string();
-                    let int_number: usize = usize::from_str_radix(
-                        title.split("x").collect::<Vec<&str>>().get(1).unwrap(),
-                        16,
-                    )
-                    .unwrap();
-                    /// We compute interrupts number that are implemented
-                    int_defined.push(int_number);
+                    // Ignore naked wrappers
+                    if title.find("naked").is_none() {
+                        let int_number: usize = usize::from_str_radix(
+                            title.split("x").collect::<Vec<&str>>().get(1).unwrap(),
+                            16,
+                        )
+                        .unwrap();
+                        // We compute interrupts number and append it to the int_defined list
+                        int_defined.push(int_number);
+                    }
                 }
             }
             _ => {}
@@ -195,26 +231,40 @@ pub fn interrupt_default(
 
     let mut default_interrupts = Vec::new();
 
-    /// Auto implement other interrupts with default template
-    for i in 0..256{
+    // Auto implement other interrupts with default template and a wrapper
+    for i in 0..256 {
         if !int_defined.contains(&i) {
-            let name = format!("_int0x{:x}", i);
+            let name = format!("_wrapped_int0x{:x}", i);
             let n = i as u32;
             let ident = Ident::new(name.as_str(), Span::mixed_site());
-            let section = name.replace("_", ".");
+            let section = String::from(".int");
             let default_int = quote! {
                 #[no_mangle]
                 #[link_section = #section]
-                pub fn #ident() {
+                pub fn #ident (){
                     let int_code : u32 = #n;
                     #(#default_body)*
-                    unsafe {
-                        asm!(
-                            "iret"
-                        )
-                    };
                 }
             };
+            let naked_name = format!("_int0x{:x}", i);
+            let naked_ident = Ident::new(naked_name.as_str(), Span::mixed_site());
+            let wrapper = format!(
+                "pushad
+                call {}
+                popad
+                iretd",
+                name
+            );
+            let naked_int = quote! {
+                #[link_section = #section]
+                #[naked]
+                pub fn #naked_ident() {
+                    unsafe {asm!(
+                        #wrapper
+                    , options(noreturn))}
+                }
+            };
+            default_interrupts.push(naked_int);
             default_interrupts.push(default_int);
         }
     }
@@ -223,6 +273,5 @@ pub fn interrupt_default(
         #(#default_interrupts)*
     };
 
-    //panic!("{}", stream.to_string());
     stream.into()
 }
