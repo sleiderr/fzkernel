9f64fac14104f4c4e0484ef3c6d625cc395655eb
Sacha Dupuydauby <gh.4eczg@slmail.me>
2023-08-13 18:14:37 +0200

    refactor: file structure (#39)
    * refactor: file structure

---
+++ build/src/main.rs
@@ -0,0 +1,48 @@
+use std::error::Error;
+use std::fs;
+use std::io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write};
+use std::path::Path;
+use std::vec::Vec;
+
+fn write_to_disk(path: &Path) -> Result<(), Box<dyn Error>> {
+    let disk_img = fs::OpenOptions::new()
+        .write(true)
+        .truncate(false)
+        .create(true)
+        .open(path)?;
+
+    let mut writer = BufWriter::new(&disk_img);
+
+    println!("Burn MBR to disk image");
+
+    let mbr = fs::File::open("target/mbr/x86_64-fbios/release/mbr.bin")?;
+    let buff = BufReader::new(mbr)
+        .bytes()
+        .collect::<io::Result<Vec<u8>>>()?;
+    writer.seek(SeekFrom::Start(0))?;
+    writer.write_all(&buff)?;
+
+    println!("MBR done !");
+
+    println!("Burn bootloader to disk image");
+    let mbr = fs::File::open("target/real/x86_64-fbios/release/real.bin")?;
+    let buff = BufReader::new(mbr)
+        .bytes()
+        .collect::<io::Result<Vec<u8>>>()?;
+    writer.write_all(&buff)?;
+
+    let mbr = fs::File::open("target/main/x86_64-fbios/release/main.bin")?;
+    let buff = BufReader::new(mbr)
+        .bytes()
+        .collect::<io::Result<Vec<u8>>>()?;
+    writer.write_all(&buff)?;
+
+    println!("Bootloader done !");
+
+    Ok(())
+}
+
+fn main() {
+    let disk_path: &Path = Path::new("./boot.img");
+    write_to_disk(disk_path).unwrap();
+}
---
+++ src/x86/mod.rs
@@ -0,0 +1,29 @@
+pub mod cpuid;
+pub mod flags;
+pub mod msr;
+pub mod tsc;
+
+#[cfg(feature = "alloc")]
+pub mod idt;
+
+pub mod int {
+    use core::arch::asm;
+    pub fn interrupts_disabled() -> bool {
+        let flags = super::flags::Flags::read();
+        flags.ipts_disabled()
+    }
+
+    #[inline]
+    pub fn disable_interrupts() {
+        unsafe {
+            asm!("cli");
+        }
+    }
+
+    #[inline]
+    pub fn enable_interrupts() {
+        unsafe {
+            asm!("sti");
+        }
+    }
+}
