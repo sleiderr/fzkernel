74e7726aa1ce2b6307ee3dc34a945967abc2de56
Sacha <sacha.dupuydauby@student-cs.fr>
2023-11-03 01:11:48 +0100

    fix(ps2): fixed outb / inb impl

--- src/io/ps2/mod.rs
+++ src/io/ps2/mod.rs
@@ -1,34 +1,17 @@
 use core::arch::asm;
 
+use crate::io::{inb, outb};
+
 pub fn send_data(data: u8) {
-    unsafe {
-        asm!(
-        "out 0x60, {}",
-        in(reg_byte) data
-        );
-    }
+    outb(0x60, data);
 }
 
 pub fn read_ps2() -> u8 {
-    let data: u8;
-
-    unsafe {
-        asm!(
-        "in {}, 0x60",
-        out(reg_byte) data
-        );
-    }
-
-    data
+    inb(0x60)
 }
 
 pub fn send_ps2(cmd: u8) {
-    unsafe {
-        asm!(
-        "out 0x64, {}",
-        in(reg_byte) cmd
-        );
-    }
+    outb(0x64, cmd);
 }
 
 pub fn input_wait(mut loops: u16) -> Result<(), ()> {
